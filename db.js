69111ef16044cd17f598ad55d775b5dc7e318cd5
Kaloow13 <162516775+fanlocklear@users.noreply.github.com>
2024-03-13 11:13:22 -0400

    création bd client et coiffeur

--- db.js
+++ db.js
@@ -4,26 +4,96 @@ const knex = require('knex');
 const db = knex({
   client: 'sqlite3',
   connection: {
-    filename: './utilisateurs.sqlite3', // Le chemin vers votre fichier SQLite3
+    filename: './salonDeCoiffure.sqlite3', // Le chemin vers votre fichier SQLite3
   },
   useNullAsDefault: true,
 });
 
-//Création de la table Utilisateurs, information ici
-  //db.schema.createTable('utilisateurs', (table) => {
-  //table.increments('id').primary();
-  //table.string('nom');
-  //table.string('prenom');
-  //table.integer('age');
-  //table.string('email');
-  //table.string('numeroTelephone');
-  //})
-//.then(() => {
-  //console.log('Table "utilisateurs" créée avec succès.');
-//})
-//.catch((error) => {
-  //console.error('Erreur lors de la création de la table "utilisateurs":', error);
-//});
+
+// Création de la table Coiffeur
+db.schema.createTable('Coiffeur', table => {
+  table.increments('idCoiffeur').primary();
+  table.string('nomCoiffeur').notNullable();
+  table.string('prenomCoiffeur').notNullable();
+  table.integer('numCoiffeur').notNullable();
+  table.integer('idSalon').unsigned();
+  table.integer('idService').unsigned();
+  table.integer('idPortfolio').unsigned();
+  table.integer('idDisponibilite').unsigned();
+  table.foreign('idSalon').references('Salon.idSalon');
+  table.foreign('idService').references('Service.idService');
+  table.foreign('idPortfolio').references('Portfolio.idPortfolio');
+  table.foreign('idDisponibilite').references('Disponibilite.idDisponibilite');
+}).then(() => console.log('Table Coiffeur créée'));
+
+// Création de la table Coiffeur
+db.schema.createTable('Client', table => {
+  table.increments('idClient').primary();
+  table.string('nomClient').notNullable();
+  table.string('prenomClient').notNullable();
+  table.integer('numClient').notNullable();
+  table.integer('idSalon').unsigned();
+  table.integer('idService').unsigned();
+  table.integer('idPortfolio').unsigned();
+  table.foreign('idSalon').references('Salon.idSalon');
+  table.foreign('idService').references('Service.idService');
+  table.foreign('idPortfolio').references('Portfolio.idPortfolio');
+}).then(() => console.log('Table Client créée'));
+
+// Création de la table RendezVous
+db.schema.createTable('rendezvous', table => {
+  table.increments('idRendezvous').primary();
+  table.date('dateRendezvous').notNullable();
+  table.integer('heureRendezvous').notNullable();
+  table.integer('idClient').unsigned();
+  table.foreign('idClient').references('Client.idClient');
+  table.integer('idCoiffeur').unsigned();
+  table.foreign('idCoiffeur').references('Coiffeur.idCoiffeur');
+}).then(() => console.log('Table Rendezvous créée'));
+
+// Création de la table Salon
+db.schema.createTable('Salon', table => {
+  table.increments('idSalon').primary();
+  table.string('nomSalon').notNullable();
+  table.string('adresseSalon').notNullable();
+  table.string('emailSalon').notNullable();
+  table.integer('numSalon').notNullable();
+}).then(() => console.log('Table Salon créée'));
+
+// Création de la table InscriptionCoiffeur
+db.schema.createTable('InscriptionCoiffeur', table => {
+  table.increments('idInscription').primary();
+  table.string('token').notNullable();
+  table.integer('idCoiffeur').unsigned();
+  table.foreign('idCoiffeur').references('Coiffeur.idCoiffeur');
+}).then(() => console.log('Table InscriptionCoiffeur créée'));
+
+// Création de la table Service
+db.schema.createTable('Service', table => {
+  table.increments('idService').primary();
+  table.string('nom').notNullable();
+  table.string('description').notNullable();
+  table.integer('idCoiffeur').unsigned();
+  table.foreign('idCoiffeur').references('Coiffeur.idCoiffeur');
+}).then(() => console.log('Table Service créée'));
+
+// Création de la table Portfolio
+db.schema.createTable('Portfolio', table => {
+  table.increments('idPortfolio').primary();
+  table.string('urlPhoto').notNullable();
+  table.integer('idCoiffeur').unsigned();
+  table.foreign('idCoiffeur').references('Coiffeur.idCoiffeur');
+}).then(() => console.log('Table Portfolio créée'));
+
+// Création de la table Disponibilite
+db.schema.createTable('Disponibilite', table => {
+  table.increments('idDisponibilite').primary();
+  table.date('dateDisponibilite').notNullable();
+  table.integer('heureDisponibilite').notNullable();
+  table.integer('idCoiffeur').unsigned();
+  table.foreign('idCoiffeur').references('Coiffeur.idCoiffeur');
+}).then(() => console.log('Table Disponibilite créée'));
+
 
 //exporation de cette base de donnée
 module.exports = db;
