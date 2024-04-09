dcd50ad9eae580a627f6834fc264c1235792bf7e
MikeDudley514 <152577644+MikeDudley514@users.noreply.github.com>
2024-04-09 18:55:15 -0400

    Update connexion.js

--- public/connexion.js
+++ public/connexion.js
@@ -56,7 +56,8 @@ document.addEventListener("DOMContentLoaded", function () {
         alert("Connexion réussie ! Bienvenue, " + email);
         // Redirect to the appropriate page based on user type
         if (loginType === "client") {
-          window.location.href = "/pageAcceuil.html";
+          //window.location.href = "/pageAcceuil.html";
+          console.log("Token :", data.token);
         } else {
           window.location.href = "/pageAcceuil.html";
         }
