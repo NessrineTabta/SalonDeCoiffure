a13f68c7966dcedc1fc9518f8dc378699dde2fda
cjfromthez <144188759+cjfromthez@users.noreply.github.com>
2024-04-10 00:08:32 -0400

    probleme regler d inscription coiffeur
    .

--- serverClient.js
+++ serverClient.js
@@ -400,60 +400,50 @@ router.post("/salon", async (req, res) => {
   }
 });
 
-
 // GET: Obtenir un salon par son ID à partir du corps de la requête
-router.get('/salon', async (req, res) => {
+router.get("/salon", async (req, res) => {
   try {
     const { id } = req.body;
 
     // Vérifier si l'ID du salon est présent dans le corps de la requête
     if (!id) {
-      return res.status(400).json({ message: "L'ID du salon est requis dans le corps de la requête." });
+      return res.status(400).json({
+        message: "L'ID du salon est requis dans le corps de la requête.",
+      });
     }
 
     // Requête pour obtenir le salon par son ID
-    const salon = await db('Salon').select('nomSalon').where('idSalon', id).first();
+    const salon = await db("Salon")
+      .select("nomSalon")
+      .where("idSalon", id)
+      .first();
 
     // Vérifier si le salon existe
     if (!salon) {
-      return res.status(404).json({ message: 'Salon non trouvé.' });
+      return res.status(404).json({ message: "Salon non trouvé." });
     }
 
     res.status(200).json({ nomSalon: salon.nomSalon });
   } catch (error) {
     console.error(error);
-    res.status(500).json({ message: "Une erreur s'est produite lors de la récupération du salon." });
+    res.status(500).json({
+      message: "Une erreur s'est produite lors de la récupération du salon.",
+    });
   }
 });
 
 // GET: Route pour récupérer TOUT les salons disponibles
 router.get("/nomsSalons", async (req, res) => {
   try {
-    const nomsSalons = await db.select("nomSalon").from("Salon");
+    // Make sure to select both `idSalon` and `nomSalon` from the `Salon` table
+    const nomsSalons = await db.select("idSalon", "nomSalon").from("Salon");
     res.status(200).json(nomsSalons);
   } catch (error) {
     console.error("Une erreur s'est produite:", error);
-    res.status(500).json({ message: "Erreur lors de la récupération des noms de salons." });
+    res
+      .status(500)
+      .json({ message: "Erreur lors de la récupération des noms de salons." });
   }
 });
-
-// GET: Route pour obtenir l'ID du salon à partir de son nom
-router.get("/idSalon", async (req, res) => {
-  try {
-    const { nomSalon } = req.body; // Récupération du nom du salon depuis le corps de la requête
-    const salon =await db('Salon').select('idSalon').where('nomSalon', nomSalon).first();
-    if (salon) {
-      res.status(200).json(salon.idSalon);
-    } else {
-      res.status(404).json({ message: "Salon non trouvé." });
-    }
-  } catch (error) {
-    console.error("Une erreur s'est produite:", error);
-    res.status(500).json({ message: "Erreur lors de la recherche de l'ID du salon." });
-  }
-});
-
-
-
 
 module.exports = router;
