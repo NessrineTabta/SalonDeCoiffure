61e5dcb8a0c1901a95d16f24868be179140e86b0
MikeDudley514 <152577644+MikeDudley514@users.noreply.github.com>
2024-04-14 19:53:42 -0400

    ancienne ver de afficher avis

--- public/AfficherAvis/afficheravis.js
+++ public/AfficherAvis/afficheravis.js
@@ -1,6 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-  document.getElementById('btnDeconnexion').addEventListener('click', deconnexion);
-
   fetch("/avis")
     .then((response) => response.json())
     .then((avis) => {
@@ -45,7 +43,6 @@ document.addEventListener("DOMContentLoaded", () => {
     );
 });
 
-// pour generer les etoiles
 function generateStars(nombreEtoile) {
   let starsHTML = "";
   for (let i = 0; i < nombreEtoile; i++) {
@@ -55,51 +52,4 @@ function generateStars(nombreEtoile) {
     starsHTML += '<i class="far fa-star"></i>';
   }
   return starsHTML;
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  const token = sessionStorage.getItem("token");
-  //redirection si pas de compte
-  if (!token) {
-    window.location.href = "../connexion.html";
-    return;
-  }
-
-  const loginType = sessionStorage.getItem("loginType");
-  updateNavigationBar(loginType);
-
-  document.getElementById('btnDeconnexion').addEventListener('click', deconnexion);
-});
-
-// changer navbar en fonction du type de user (client ou coiffeur)
-function updateNavigationBar(loginType) {
-  let navContent = "";
-  const navContainer = document.querySelector(".nav .nav-items");
-  const prendreRdv = document.getElementById('btnPriseRdv');
-
-  if (loginType === "client") {
-    navContent = `
-    <a href="../accueil/accueil.html#section-about">À propos</a>
-    <a href="../accueil/accueil.html#section-contact">Contact</a>
-        <a href="../avis.html">Avis</a>
-        <a href="../AfficherAvis/afficherAvis.html">Tous les avis</a>
-        <a href="../rendezvousClient/rendezvousClient.html">Mes rendez-vous</a>
-        `;
-  } else if (loginType === "coiffeur") {
-    prendreRdv.style.display = 'none'
-    navContent = `
-        <a href="../CoiffeurProfil/portfolio.html">Profil</a>
-        <a href="../AfficherAvis/afficherAvis.html">Tous les avis</a>
-        <a href="../rendezvousCoiffeur/rendezvousCoiffeur.html">Afficher mes rendez vous</a>
-        `;
-  }
-
-  // Mettre à jour le contenu de la barre de navigation
-  navContainer.innerHTML = navContent;
-}
-
-
-function deconnexion() {
-  sessionStorage.removeItem("token");
-  window.location.href = "../connexion.html";
 }
